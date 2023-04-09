8694aeafca9b5bfd9ceb9e030577a096010b8d14
Вадим <itamiomw@gmail.com>
2023-04-09 17:00:04 +0300

    initial setup

--- app/src/main/java/com/example/orderappcleanarchitecture/MainActivity.kt
+++ app/src/main/java/com/example/orderappcleanarchitecture/MainActivity.kt
@@ -3,13 +3,6 @@ package com.example.orderappcleanarchitecture
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.orderappcleanarchitecture.ui.theme.OrderAppCleanArchitectureTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +10,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             OrderAppCleanArchitectureTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    OrderAppCleanArchitectureTheme {
-        Greeting("Android")
-    }
-}
